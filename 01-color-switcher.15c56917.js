6522cd4957387234243e9b4c7bc5dfeaafa07ae1
oleksiiPut <94678751+oleksiiPut@users.noreply.github.com>
2022-06-22 16:43:07 +0000

    Deploying to gh-pages from @ oleksiiPut/goit-js-hw-09@26d08bf41a639c1d83ba9b8f2a68610006bd11b2 🚀

---
+++ 01-color-switcher.15c56917.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a;t.addEventListener("click",(()=>{a&&clearInterval(a),a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.15c56917.js.map
